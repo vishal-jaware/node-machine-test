3d6e52215f8894bd476d12ffd75710b1624f9829
vishal-jaware <vishaljaware10@gmail.com>
2026-01-29 19:26:47 +0530

    db.js update for deploy

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root", 
-  database: "machine_test"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "machine_test",
+  port: process.env.DB_PORT || 3306
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
-    console.error("DB connection error:", err.message);
-    return;
+    console.error("❌ MySQL connection failed:", err.message);
+  } else {
+    console.log("✅ MySQL Connected");
   }
-  console.log("MySQL Connected");
 });
 
 module.exports = db;
